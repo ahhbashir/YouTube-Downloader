d5a4a6cf5589bd91f4a2710453f781e66424b6f3
Bashir Ahmed <bashir.ahmed@duke.edu>
2022-07-31 14:25:07 -0400

    first commit

---
+++ ytDownloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+
+print("Views:", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download(r'C:\Users\bashi\OneDrive\Documents\DownloadedVideos') #Make sure to change this line to your own directory
